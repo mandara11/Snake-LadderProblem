02ad355e62a0006e0d756eadfd2740ee92998e04
mandara11 <mandaram2345@gmail.com>
2022-07-31 22:16:34 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Sanke and Ladder Problem");
+        }
+    }
+}
