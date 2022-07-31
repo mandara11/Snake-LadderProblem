192a1b8e9205d105505c73554ad406dfefe24f0b
mandara11 <mandaram2345@gmail.com>
2022-07-31 22:25:24 +0530

    SinglePlayer

---
+++ SnakeLadderGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder//UC1
+{
+    public static class SnakeLadderGame
+    {
+        const int position = 0;
+        public static void SinglePlayer()
+        {
+            Console.WriteLine("The starting position is -> "+position);
+        }
+    }
+}
