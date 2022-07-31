ec0eca735e4b6b450468905f2c9c64fb81564433
mandara11 <mandaram2345@gmail.com>
2022-07-31 22:33:33 +0530

    Getnumberone to six

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public static class Player
+    {
+        const int position = 0;
+        public static void CheckDieRoll()
+        {
+            Console.WriteLine("The starting position is -> "+position);
+            Random random = new Random();
+            int DiePosition = random.Next(1, 7);
+            Console.WriteLine("Die Number is -> : " + DiePosition);
+        }
+    }
+}
