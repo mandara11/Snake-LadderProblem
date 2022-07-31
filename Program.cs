22b4be3e16326c90530a2b415e396b32b82844e6
mandara11 <mandaram2345@gmail.com>
2022-07-31 22:34:15 +0530

    Added:UC2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Problem");
-            SnakeLadderGame.SinglePlayer();
+            SnakeLadderGame.SinglePlayer(); //UC1
+            Player.CheckDieRoll(); //UC2
         }
     }
 }
